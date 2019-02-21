9583e9f526beb41316dd5e783b7146273bcf0538
Antoinette Lee <antoinette.lee02@myhunter.cuny.edu>
2019-02-21 18:19:02 +0000

    Automatically backed up by Learn

--- lib/school.rb
+++ lib/school.rb
@@ -1,18 +1,27 @@
 class School
-  
+
   def initialize(school_name)
     @school_name = school_name
     @roster = {}
   end
-  
+
   def roster
     @roster
   end
-  
+
   def add_student(student_name, grade_number)
-    @roster[grade_number] = []
+    @roster[grade_number] ||= []
     @roster[grade_number] << student_name
-    @roster
   end
-  
+
+  def grade(grade_number)
+    @roster[grade_number]
+  end
+
+  def sort
+    @roster.each do |grade_number, students_array|
+      students_array.sort!
+    end
+  end
+
 end
